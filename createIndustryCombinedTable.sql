7317f17f6dff500d3ba6c4b29543c9143f152600
jwmcnichol <65105444+jwmcnichol@users.noreply.github.com>
2024-12-04 22:38:53 -0500

    Update createIndustryCombinedTable.sql

--- createIndustryCombinedTable.sql
+++ createIndustryCombinedTable.sql
@@ -1,55 +1,27 @@
---BEGIN TRANSACTION;
 
+select 
+	iap.activityID,
+	iap.productTypeID,
+	iap.quantity,
+	iap.typeID AS iapBlueprintID,
+	iam.quantity AS materialQty,
+	iam.materialTypeID,
+	pt.typeName AS productName,
+	mt.typeName AS materialName,
+	igp.groupName AS productGroupName
+from 
+	[dbo].[industryActivityProducts] iap
+left join 
+	[dbo].[industryActivityMaterials] iam 
+	on iam.typeID = iap.typeID
+LEFT JOIN 
+    [dbo].[invTypes] pt 
+    ON pt.typeID = iap.productTypeID
+LEFT JOIN 
+    [dbo].[invTypes] mt 
+    ON mt.typeID = iam.materialTypeID
+LEFT JOIn 
+	invGroups igp 
+	ON igp.groupID = pt.groupID
 
-USE [eve.SDEmount.full.Current]
-CREATE TABLE [dbo].[industryCombined] (
-    typeID INT,
-    activityID INT,
-    typeName NVARCHAR(255),
-    groupName NVARCHAR(255),
-    materialTypeID INT,
-	materialQuantity INT
-	
-); 
-
--- Step 2: Populate the new table with data from the joined tables
-INSERT INTO [dbo].[industryCombined] 
-(
-    typeID, 
-    activityID, 
-    typeName, 
-    groupName, 
-    materialTypeID,
-	materialQuantity
-)
-
-sELECT 
-    itm.typeID,
-	iam.activityID AS activityID,
-    it.TYPENAME AS typeName,
-    ig.groupName AS groupName,
-    itm.materialTypeID,
-	itm.quantity AS materialQuantity
-FROM 
-    [dbo].[invTypeMaterials] itm
-left JOIN 
-    [dbo].[invTypes] it ON itm.typeID = it.typeID
-left JOIN 
-	[dbo].[industryActivityMaterials] iam on itm.typeID = iam.materialTypeID
-left JOIN 
-    [dbo].[invGroups] ig ON it.groupID = ig.groupID;
-
---IF @@ERROR = 0
---    COMMIT TRANSACTION;  
---ELSE
---    ROLLBACK TRANSACTION; 
-
-
---ALTER TABLE industrycombined ADD materialName NVARCHAR(255);
-/*
-UPDATE industryCombined
-SET materialName = it.typeName
-FROM industryCombined yt
-LEFT JOIN invTypes it
-ON yt.materialTypeID = it.typeID;
-*/
+where iap.typeID = 17844
